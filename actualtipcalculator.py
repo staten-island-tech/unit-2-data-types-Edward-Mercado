26f538c558727ae095ab1aa567cf9235d7f76ba2
Edward-Mercado <edwardmercado149@gmail.com>
2024-09-16 09:20:03 -0400

    Update actualtipcalculator.py

---
+++ actualtipcalculator.py
@@ -0,0 +1,8 @@
+basevalue = float(input("How much was your meal?"))
+tipvalue = float(input("How much would you like to tip?"))
+
+actualtipvalue = tipvalue + 1
+
+total = actualtipvalue*basevalue
+
+print (total)
