804c814c6d5052932e06f228a8da88dbcdc4713c
Edward-Mercado <edwardmercado149@gmail.com>
2024-09-17 09:20:19 -0400

    man

---
+++ actualactualtipcalculator.py
@@ -0,0 +1,18 @@
+
+basevalue = float(input("How much was your meal?"))
+tipvalue = (input("How was your service today?"))
+
+if tipvalue == "bad":
+    badtotal = basevalue*1.05
+    print ("Your total is" + " " + str(badtotal))
+elif tipvalue == "okay":
+    okaytotal = basevalue*1.1
+    print ("Your total is" + " " + str(okaytotal))
+elif tipvalue == "good":
+    goodtotal = basevalue*1.15
+    print ("Your total is" + " " + str(goodtotal))
+elif tipvalue == "spectacular":
+    spectaculartotal = basevalue*1.1
+    print ("Your total is" + " " + str(spectaculartotal))
+else:
+    print ("Your total is" + " " + str(basevalue))
--- actualtipcalculator.py
+++ actualtipcalculator.py
@@ -1,8 +1,9 @@
+
 basevalue = float(input("How much was your meal?"))
 tipvalue = float(input("How much would you like to tip?"))
 
-actualtipvalue = tipvalue + 1
+actualtipvalue = tipvalue+1
 
 total = actualtipvalue*basevalue
 
-print (total)
+print ("Your total is" + " " + str(total))
