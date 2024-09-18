590b07c0c9c790979afc13bd9c60c40b77ff85e8
Edward-Mercado <edwardmercado149@gmail.com>
2024-09-18 09:22:12 -0400

    9/18
    im so tired

--- tipcalculator.py
+++ tipcalculator.py
@@ -1,14 +1,3 @@
 number = input("How much would you like to tip?")
-if number == "0%":
-    print ("1")
-elif number == "25%":
-    print ("1.25")
-elif number == "5%":
-    print ("1.05")
-elif number == "10%":
-    print ("1.10")
-else:
-    print ("1")
 
-subtotal = input("How much have you paid?")
 
