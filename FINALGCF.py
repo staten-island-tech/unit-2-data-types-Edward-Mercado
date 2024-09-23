b734ed2c86a81b42f877d548640aee9fd2dc9aa4
Edward-Mercado <edwardmercado149@gmail.com>
2024-09-23 08:46:03 -0400

    finallyITSdone

--- FINALGCF.py
+++ FINALGCF.py
@@ -2,7 +2,7 @@ number1 = int(input("Provide the first number."))
 number2 = int(input("Provide the second number."))
 
 commonfactors = {1}
-for x in range(2, number1):
+for x in range(2, (min(number1,number2) + 1)):
     if number1 % x == 0 and number2 % x == 0:
         commonfactors.add(x)
 
